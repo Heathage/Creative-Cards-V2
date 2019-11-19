b016f5bd688779f11df2dea9bd8a606026ca8b90
James Heath <jh230155@falmouth.ac.uk>
2019-11-19 16:00:37 +0000

    Created script to have arm follow player character. Created script to point and shoot, with LMB, towards mouse pointer.

---
+++ CreativeCards-master/Creativity Cards Game - Team 11/Assets/Scripts/ArmFollow.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ArmFollow : MonoBehaviour
+{
+    float min = 0;
+    float max = 0;
+    public Transform target;
+    private Vector3 offset;
+
+    void Start()
+    {
+        offset = this.transform.position - target.position;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (target != null)
+        {
+            {
+                this.transform.position = offset + target.position;
+
+                //this.transform.position = new Vector3(transform.position.x, transform.position.y, transform.position.z);
+            }
+        }
+
+    }
+}
+
---
+++ CreativeCards-master/Creativity Cards Game - Team 11/Assets/Scripts/Shooting.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Shooting : MonoBehaviour
+{
+    [SerializeField]
+    private Transform Gun;
+
+    [SerializeField]
+    private GameObject Bullet;
+
+    private Vector2 lookDirection;
+    private float lookAngle;
+
+    // Update is called once per frame
+    void Update()
+    {
+        lookDirection = Camera.main.ScreenToWorldPoint(Input.mousePosition) - transform.position;
+        lookAngle = Mathf.Atan2(lookDirection.y, lookDirection.x) * Mathf.Rad2Deg;
+        transform.rotation = Quaternion.Euler(0f, 0f, lookAngle - 90f);
+
+        if (Input.GetMouseButtonDown(0))
+        {
+            FireBullet();
+        }
+    }
+
+    private void FireBullet()
+    {
+        GameObject firedBullet = Instantiate(Bullet, Gun.position, Gun.rotation);
+        firedBullet.GetComponent<Rigidbody2D>().velocity = Gun.up * 10f;
+    }
+}
